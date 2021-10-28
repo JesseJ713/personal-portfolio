7c78a44483e15daf26da6687a5315ee077aae59b
JCJ <jesse.jackson.c@gmail.com>
2021-10-28 18:14:19 -0500

    Added personalized hero section with button that needs a call to action

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Full Stack Developer with experience in Business Analysis
+      </SectionText>
+      <Button>Contact Me</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
