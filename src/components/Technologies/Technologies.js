c5f9f870555a09c2262830b1278acd6c3b57bf66
JCJ <jesse.jackson.c@gmail.com>
2021-10-28 20:13:17 -0500

    Adding the actual technologies to the section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -17,11 +17,46 @@ import {
 const Technologies = () => (
   <Section id='tech'>
     <SectionDivider />
+    <br />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
       I've worked with a wide range of technologies <br /> from Front-End to
       Back-End
     </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size='3rem' />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with: <br />
+            React.js <br /> Next.js <br />
+            JavaScript <br /> HTML5 <br /> CSS3 <br /> Sass
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size='3rem' />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with: <br />
+            Node <br /> Express <br /> mySQL <br /> PostgreSQL <br />
+            MongoDB
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size='3rem' />
+        <ListContainer>
+          <ListTitle>QA</ListTitle>
+          <ListParagraph>
+            Experience with: <br />
+            Jest Testing <br /> Version Control <br /> Git
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
   </Section>
 );
 
